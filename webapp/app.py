dd259f388cd9084763e7d13458d520ec3821fef9
shihao <shihao1007@gmail.com>
2019-06-15 18:52:40 -0500

    x

--- webapp/app.py
+++ webapp/app.py
@@ -12,7 +12,7 @@ app = Flask(__name__)
 def index():
     
     print("BEFORE bokeh server_document")
-    vggm = server_document(url="http://0.0.0.0:5006/map")
+    vggm = server_document(url="http://127.0.0.1:5006/map")
     print()
     print("AFTER !!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
     print(vggm)
