fe7504a129d3a8f2548e7ddfffccc589c220db56
shihao <shihao1007@gmail.com>
2019-06-15 19:40:52 -0500

    x

--- webapp/app.py
+++ webapp/app.py
@@ -3,24 +3,19 @@ from flask import Flask, render_template
 from bokeh.embed import server_session
 from bokeh.client import pull_session
 from bokeh.embed import server_document
+import sys, os
 
+IP = os.environ["BK_IP"]
 # instantiate the app
 app = Flask(__name__)
 
 # render the template
 @app.route('/')
 def index():
-    
-    print("BEFORE bokeh server_document")
-    vggm = server_document(url="http://bokehapp:5006/map")
-    print()
-    print("AFTER !!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
-    print(vggm)
-    print()
-    print()
-    print("******************************************")
+
+    vggm = server_document(url="http://" + IP + ":5006/map")
 
     return render_template("index.html", vggm=vggm)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", debug=True)
+    app.run(host="0.0.0.0")
